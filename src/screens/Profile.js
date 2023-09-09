e2d31c1e1a8281016afab5260c0a9d549b1697b1
gabrielardevol <artsdevol@gmail.com>
2023-09-09 20:30:10 +0200

    fontawesome icon

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,11 +1,27 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 import pfpImage from '../../assets/favicon.png'
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faPen } from '@fortawesome/free-solid-svg-icons/faPen'
+
+
+
 function ProfileScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <View style={[styles.container, { width: 200, height: 200 }]}>
-      <Image source={pfpImage} style={styles.image} />
+      <View >
+        <View style={[styles.container, { width: 150, height: 150 }]}>      
+          <Image source={pfpImage} style={styles.image} />
+        </View>
+      {/* <Image source={pfpImage} style={styles.imageButton} /> */}
+      <View style={[styles.imageButton, { width: 50, height: 50 }]}>   
+        <FontAwesomeIcon icon={ faPen } size={ 28 } />   
       </View>
+
+
+      </View>
+
 
       <View style={{ flex: 1 }}>
         <Text>Hola</Text>
@@ -21,13 +37,28 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
-    width: 200,
-    height: 200,
+    width: 150,
+    height: 150,
     resizeMode: 'contain',
     backgroundColor: 'red',
     borderRadius: 5000
-
   },
+  imageButton: {
+    flex: 1,
+    width: 60,
+    height: 60,
+    resizeMode: 'contain',
+    backgroundColor: 'gainsboro',
+    borderRadius: 50,
+    position: 'absolute',
+    bottom: -10,
+    right: -10,
+    justifyContent: 'center',
+    alignItems: 'center'
+
+    
+    
+  }
 });
 
 export default ProfileScreen;

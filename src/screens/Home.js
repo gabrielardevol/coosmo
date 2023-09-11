927bc613a3cb545e0becb15b64ca59a770c15045
gabrielardevol <artsdevol@gmail.com>
2023-09-12 00:52:48 +0200

    homepage

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,17 +3,28 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Drawer = createDrawerNavigator();
 
-function HomeScreen({ navigation }) {
+// function HomeScreen({ navigation }) {
+//     return (
+//       <View style={{ flex: 1, justifyContent: 'flex-end', backgroundColor: "red" }} >
+//         <View style={{backgroundColor: "green", alignItems: 'flex-end'}}>
+//         </View>
+//         <View style={{backgroundColor: "pink", flex: 1,  alignItems: 'center', justifyContent: "center"}}>
+//         <Text>Home Screen (options: headerShown: false)</Text>
+//         </View>
+//       </View>
+//     );
+// }
+
+  import { WebView } from 'react-native-webview';
+
+  function HomeScreen() {
     return (
-      <View style={{ flex: 1, justifyContent: 'flex-end', backgroundColor: "red" }} >
-        <View style={{backgroundColor: "green", alignItems: 'flex-end'}}>
-        </View>
-        <View style={{backgroundColor: "pink", flex: 1,  alignItems: 'center', justifyContent: "center"}}>
-        <Text>Home Screen (options: headerShown: false)</Text>
-        </View>
-      </View>
+    <WebView
+      source={{ uri: 'https://orbits-frontend.vercel.app/' }} // or use 'html' property for inline HTML
+      style={{ flex: 1 }} // adjust the style as needed
+    />
     );
-}
+  };
 
 
 function HomeDrawer() {

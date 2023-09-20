64c48a9d4a03517ed6db916027541a2c5da166f2
gabrielardevol <artsdevol@gmail.com>
2023-09-20 10:48:18 +0200

    dropdown implemented

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,4 +1,6 @@
 import { StyleSheet, Text, View, Button, TouchableOpacity, TextInput } from 'react-native';
+import React, { useState } from 'react';
+
 import { Formik } from 'formik';
 import Input from '../components/Form';
 
@@ -73,6 +75,71 @@ export  const SignupScreen2 = ({navigation}) => {
     );
   };
 
+  import { Dropdown } from 'react-native-element-dropdown';
+
+  const data = [
+    { label: 'Item 1', value: '1' },
+    { label: 'Item 2', value: '2' },
+    { label: 'Item 3', value: '3' },
+    { label: 'Item 4', value: '4' },
+    { label: 'Item 5', value: '5' },
+    { label: 'Item 6', value: '6' },
+    { label: 'Item 7', value: '7' },
+    { label: 'Item 8', value: '8' },
+  ];
+
+  const DropdownComponent = () => {
+    const [value, setValue] = useState(null);
+    const [isFocus, setIsFocus] = useState(false);
+
+    const renderLabel = () => {
+      if (value || isFocus) {
+        return (
+          <Text style={[styles.label, isFocus && { color: 'blue' }]}>
+            Dropdown label
+          </Text>
+        );
+      }
+      return null;
+    };
+
+    return (
+      <View style={styles.container}>
+        {renderLabel()}
+        <Dropdown
+          style={[styles.textInput, {backgroundColor: "red", fontSize: 2}]}
+          // style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
+          // placeholderStyle={styles.placeholderStyle}
+          // selectedTextStyle={styles.selectedTextStyle}
+          // inputSearchStyle={styles.inputSearchStyle}
+          // iconStyle={styles.iconStyle}
+          data={data}
+          // search
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          placeholder={!isFocus ? 'Select item' : '...'}
+          searchPlaceholder="Search..."
+          value={value}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          onChange={item => {
+            setValue(item.value);
+            setIsFocus(false);
+          }}
+          // renderLeftIcon={() => (
+          //   <AntDesign
+          //     style={styles.icon}
+          //     color={isFocus ? 'blue' : 'black'}
+          //     name="Safety"
+          //     size={20}
+          //   />
+          // )}
+        />
+      </View>
+    );
+  };
+
 
   export  const SignupScreen3 = ({navigation}) => {
     return (
@@ -86,6 +153,8 @@ export  const SignupScreen2 = ({navigation}) => {
             <View style={{...styles.margin, alignSelf: "center", flex: 1, justifyContent: "center"}}>
             <Text style={{...styles.colorfulText, ...styles.h1, textAlign: "center"}}>3/3</Text>
             <CreaTuNuevaCuenta />
+
+                <DropdownComponent></DropdownComponent>
                 <Input name="name" 
                 label="Inserta tu nombre y apellidos" 
                 placeholder="Nombre" 
